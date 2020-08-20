f40312ea821b7ad979371b5f8959bb1b4ac4d4bb
Sifat Ibna Amin <1605112@ugrad.cse.buet.ac.bd>
2020-08-21 02:45:37 +0600

    Picking numbers problem

---
+++ Picking Numbers/main.py
@@ -0,0 +1,11 @@
+import sys
+from collections import Counter
+
+n = int(input().strip())
+a = [int(a_temp) for a_temp in input().strip().split(' ')]
+
+d = Counter(a)
+best = 0
+for i in range(99):
+    best = max(d[i] + d[i+1], best)
+print(best)
